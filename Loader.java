d92f111dc1722c7e4fdadc940d6d0da2041b0c63
a-moseman <drewmoseman@gmail.com>
2022-02-08 14:13:47 -0500

    Implement .load().

--- Loader.java
+++ Loader.java
@@ -1,9 +1,23 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
  * Class for reading the java file text.
  */
 public class Loader {
-    public String load(String path) {
-        // TODO: implement
-        return "";
+    public static String load(String path) {
+        StringBuilder text = new StringBuilder();
+        try {
+            File file = new File(path);
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                text.append(scanner.nextLine());
+            }
+            scanner.close();
+        } catch (FileNotFoundException exception) {
+            exception.printStackTrace();
+        }
+        return text.toString();
     }
 }
