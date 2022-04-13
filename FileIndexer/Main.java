b3bdb5eb2121758ecef0f85aa49e789d9f751bba
a-moseman <drewmoseman@gmail.com>
2022-04-13 16:50:00 -0400

    Add dataFiles and reimplmennt Query in Main.

--- FileIndexer/Main.java
+++ FileIndexer/Main.java
@@ -4,22 +4,21 @@ public class Main {
     public static void main(String[] args) {
         // TODO: check if already intially loaded
         String path = "FileIndexer//10E.csv";
-        String dirPath = "dataFiles";
+        String dirPath = "FileIndexer//dataFiles";
         String fileName = "data";
         Database db = new Database();
         db.initialBinaryLoad(path);
         db.saveBinary(dirPath, fileName);
         // finish initial set up
         db.loadBinary(dirPath, fileName);
-        System.out.println(db.getTable().getRaw());
 
-        // Query query = new Query(db.getTable());
-        // String[] resultSet = query
-        // .select("name")
-        // .whereGreaterThan("G")
-        // .getResultSet();
-        // for (String field : resultSet) {
-        // System.out.println(field);
-        // }
+        Query query = new Query(db.getTable());
+        String[] resultSet = query
+                .select("name")
+                .whereGreaterThan("G")
+                .getResultSet();
+        for (String field : resultSet) {
+            System.out.println(field);
+        }
     }
 }
