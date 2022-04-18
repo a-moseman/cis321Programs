c3c6cba74c577762d0b980c8b76aaba9427630f2
a-moseman <drewmoseman@gmail.com>
2022-04-18 13:03:23 -0400

    Add files for file compression.

---
+++ FileIndexer/ArrayOperations.java
@@ -0,0 +1,169 @@
+package FileIndexer;
+
+public class ArrayOperations {
+
+    // ___Add operations___\\
+
+    public static int[] add(int[] a, int[] b) {
+        assert a.length == b.length;
+        int[] c = new int[a.length];
+        for (int i = 0; i < c.length; i++) {
+            c[i] = a[i] + b[i];
+        }
+        return c;
+    }
+
+    public static long[] add(long[] a, long[] b) {
+        assert a.length == b.length;
+        long[] c = new long[a.length];
+        for (int i = 0; i < c.length; i++) {
+            c[i] = a[i] + b[i];
+        }
+        return c;
+    }
+
+    public static float[] add(float[] a, float[] b) {
+        assert a.length == b.length;
+        float[] c = new float[a.length];
+        for (int i = 0; i < c.length; i++) {
+            c[i] = a[i] + b[i];
+        }
+        return c;
+    }
+
+    public static double[] add(double[] a, double[] b) {
+        assert a.length == b.length;
+        double[] c = new double[a.length];
+        for (int i = 0; i < c.length; i++) {
+            c[i] = a[i] + b[i];
+        }
+        return c;
+    }
+
+    // ___Subtract Operations___\\
+
+    public static int[] subtract(int[] a, int[] b) {
+        assert a.length == b.length;
+        int[] c = new int[a.length];
+        for (int i = 0; i < c.length; i++) {
+            c[i] = a[i] - b[i];
+        }
+        return c;
+    }
+
+    public static long[] subtract(long[] a, long[] b) {
+        assert a.length == b.length;
+        long[] c = new long[a.length];
+        for (int i = 0; i < c.length; i++) {
+            c[i] = a[i] - b[i];
+        }
+        return c;
+    }
+
+    public static float[] subtract(float[] a, float[] b) {
+        assert a.length == b.length;
+        float[] c = new float[a.length];
+        for (int i = 0; i < c.length; i++) {
+            c[i] = a[i] - b[i];
+        }
+        return c;
+    }
+
+    public static double[] subtract(double[] a, double[] b) {
+        assert a.length == b.length;
+        double[] c = new double[a.length];
+        for (int i = 0; i < c.length; i++) {
+            c[i] = a[i] - b[i];
+        }
+        return c;
+    }
+
+    // ___Multiply Operations___\\
+
+    public static int[] multiply(int[] a, int[] b) {
+        assert a.length == b.length;
+        int[] c = new int[a.length];
+        for (int i = 0; i < c.length; i++) {
+            c[i] = a[i] * b[i];
+        }
+        return c;
+    }
+
+    public static long[] multiply(long[] a, long[] b) {
+        assert a.length == b.length;
+        long[] c = new long[a.length];
+        for (int i = 0; i < c.length; i++) {
+            c[i] = a[i] * b[i];
+        }
+        return c;
+    }
+
+    public static float[] multiply(float[] a, float[] b) {
+        assert a.length == b.length;
+        float[] c = new float[a.length];
+        for (int i = 0; i < c.length; i++) {
+            c[i] = a[i] * b[i];
+        }
+        return c;
+    }
+
+    public static double[] multiply(double[] a, double[] b) {
+        assert a.length == b.length;
+        double[] c = new double[a.length];
+        for (int i = 0; i < c.length; i++) {
+            c[i] = a[i] * b[i];
+        }
+        return c;
+    }
+
+    // ___Concatenate Operations___\\
+
+    public static Object[] concatenate(Object[] a, Object[] b) {
+        Object[] c = new Object[a.length + b.length];
+        System.arraycopy(a, 0, c, 0, a.length);
+        System.arraycopy(b, 0, c, a.length, b.length);
+        return c;
+    }
+
+    public static int[] concatenate(int[] a, int[] b) {
+        int[] c = new int[a.length + b.length];
+        System.arraycopy(a, 0, c, 0, a.length);
+        System.arraycopy(b, 0, c, a.length, b.length);
+        return c;
+    }
+
+    public static long[] concatenate(long[] a, long[] b) {
+        long[] c = new long[a.length + b.length];
+        System.arraycopy(a, 0, c, 0, a.length);
+        System.arraycopy(b, 0, c, a.length, b.length);
+        return c;
+    }
+
+    public static float[] concatenate(float[] a, float[] b) {
+        float[] c = new float[a.length + b.length];
+        System.arraycopy(a, 0, c, 0, a.length);
+        System.arraycopy(b, 0, c, a.length, b.length);
+        return c;
+    }
+
+    public static double[] concatenate(double[] a, double[] b) {
+        double[] c = new double[a.length + b.length];
+        System.arraycopy(a, 0, c, 0, a.length);
+        System.arraycopy(b, 0, c, a.length, b.length);
+        return c;
+    }
+
+    public static byte[] concatenate(byte[] a, byte[] b) {
+        byte[] c = new byte[a.length + b.length];
+        System.arraycopy(a, 0, c, 0, a.length);
+        System.arraycopy(b, 0, c, a.length, b.length);
+        return c;
+    }
+
+    public static boolean[] concatenate(boolean[] a, boolean[] b) {
+        boolean[] c = new boolean[a.length + b.length];
+        System.arraycopy(a, 0, c, 0, a.length);
+        System.arraycopy(b, 0, c, a.length, b.length);
+        return c;
+    }
+}
---
+++ FileIndexer/CSV.java
@@ -0,0 +1,83 @@
+package FileIndexer;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+
+public class CSV {
+    public static byte[] fieldToBytes(String field) {
+        return field.getBytes(StandardCharsets.UTF_8);
+    }
+
+    public static byte[] rowToBytes(String[] row) {
+        byte[] bytes = new byte[0];
+        for (int i = 0; i < row.length; i++) {
+            String field = row[i];
+            bytes = ArrayOperations.concatenate(bytes, fieldToBytes(field));
+            if (i < row.length - 1) {
+                bytes = ArrayOperations.concatenate(bytes, ",".getBytes(StandardCharsets.UTF_8));
+            }
+        }
+        return bytes;
+    }
+
+    public static byte[] tableToBytes(String[][] table) {
+        byte[] bytes = new byte[0];
+        for (int i = 0; i < table.length; i++) {
+            String[] row = table[i];
+            bytes = ArrayOperations.concatenate(bytes, rowToBytes(row));
+            if (i < table.length - 1) {
+                bytes = ArrayOperations.concatenate(bytes, "\n".getBytes(StandardCharsets.UTF_8));
+            }
+        }
+        return bytes;
+    }
+
+    public static String bytesToTable(byte[] bytes) {
+        return new String(bytes, StandardCharsets.UTF_8);
+    }
+
+    public static byte[] compress(String table) {
+        try {
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            GZIPOutputStream gzip = new GZIPOutputStream(out);
+            gzip.write(table.getBytes());
+            gzip.close();
+            return out.toByteArray();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static byte[] compress(String[][] table) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < table.length; i++) {
+            for (int j = 0; j < table[i].length; j++) {
+                sb.append(table[i][j]);
+                if (j < table[i].length - 1) {
+                    sb.append(',');
+                }
+            }
+            if (i < table.length - 1) {
+                sb.append('\n');
+            }
+        }
+        return compress(sb.toString());
+    }
+
+    public static String uncompress(byte[] bytes) {
+        try {
+            ByteArrayInputStream in = new ByteArrayInputStream(bytes);
+            GZIPInputStream gzip = new GZIPInputStream(in);
+            String string = bytesToTable(gzip.readAllBytes());
+            gzip.close();
+            return string;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
